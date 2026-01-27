86ffe6375d46030674689c820926139dce2ca81b
Pandurang-More <morepandurang264@gmail.com>
2026-01-27 22:21:26 +0530

    updated main file

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from langchain_huggingface import HuggingFaceEmbeddings
 from ingest import load_pdf, chunk_text
 from langchain_community.vectorstores import FAISS
-from langchain_community.embeddings import HuggingFaceEmbeddings
+#from langchain_community.embeddings import HuggingFaceEmbeddings
 
 from rag_pipeline import run_rag
 
